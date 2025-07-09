fd9f18dbde33d993212d715fa3f462bc0339f3b4
4rbshka <101452463+4rbshka@users.noreply.github.com>
2025-07-09 23:27:32 +0000

    lesson 15-20: Core functionality implementation
    - Template inheritance & static files
    - Model design & migrations
    - Complete CRUD operations
    - Custom template tags
    - Basic auth system

--- django/djlean/djlean/settings.py
+++ django/djlean/djlean/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "women.apps.WomenConfig",
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  
+    BASE_DIR / "women/static",  
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
